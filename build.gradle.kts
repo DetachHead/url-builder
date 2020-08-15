af276db478d6d9912731ddf8279e30b52fd7e93c
DetachHead <detachhead@users.noreply.github.com>
2020-08-16 01:37:17 +1000

    remove shit thats not needed

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,38 +11,19 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation("junit", "junit", "4.12")
-}
-
 kotlin {
     js(IR) { nodejs() }
     jvm {}
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-            }
-        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
         }
-        val jvmMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-            }
-        }
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
-            }
-        }
-        val jsMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-js"))
             }
         }
         val jsTest by getting {
