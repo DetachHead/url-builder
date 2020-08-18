5e767ec142f0613abb03545d0134b553455a019e
DetachHead <detachhead@users.noreply.github.com>
2020-08-18 22:44:21 +1000

    local publish tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     java
     kotlin("multiplatform") version "1.4.0"
     id("org.jetbrains.dokka") version "1.4.10"
+    id("maven-publish")
 }
 
 group = "org.example"
@@ -41,6 +42,24 @@ kotlin {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-js"))
             }
+        }
+    }
+}
+
+//this is a "local" publish i think...
+//TODO: figure out how to publish to mavencentral or something..... idk what im doing
+//  https://docs.gradle.org/current/userguide/publishing_setup.html#publishing_overview:what
+publishing {
+    publications {
+        create<MavenPublication>("urlbuilder") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "myRepo"
+            url = uri("file://${buildDir}/repo")
         }
     }
 }
