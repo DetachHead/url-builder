6315140bf4542131b785e371fb1d734d6c183401
DetachHead <detachhead@users.noreply.github.com>
2020-10-04 20:31:27 +1000

    formatting

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,10 @@
 pluginManagement {
     repositories {
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
-
         mavenCentral()
-
         maven("https://plugins.gradle.org/m2/")
         gradlePluginPortal()
         jcenter()
     }
 }
 rootProject.name = "url-builder"
-
