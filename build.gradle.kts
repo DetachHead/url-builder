a5e1e5e834fc3232ee8e85c9f2c80c5c7364872c
DetachHead <detachhead@users.noreply.github.com>
2021-04-08 00:45:14 +1000

    perhaps this is still needed?

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,3 +88,12 @@ afterEvaluate {
         }
     }
 }
+configure<PublishingExtension> {
+    publications {
+        withType<MavenPublication> {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+}
