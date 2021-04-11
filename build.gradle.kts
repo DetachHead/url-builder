c609bb3feed3aad576c15cf4a6591dc752c3684c
DetachHead <detachhead@users.noreply.github.com>
2021-04-11 18:27:21 +1000

    remmove useless shit

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,6 @@ plugins {
     `maven-publish`
 }
 
-val gitRepo = "DetachHead/url-builder"
-val gitURL = "https://github.com/$gitRepo"
-val publicationName = "url-builder"
-
 group = "io.github.detachhead"
 version = "1.0.7"
 
@@ -52,54 +48,5 @@ kotlin {
                 implementation(kotlin("test-js"))
             }
         }
-    }
-    configure(listOf(targets["metadata"], jvm(), js())) {
-        mavenPublication {
-            val targetPublication = this@mavenPublication
-            tasks.withType<AbstractPublishToMaven>()
-                .matching { it.publication == targetPublication }
-                .all { onlyIf { findProperty("isMainHost") == "true" } }
-        }
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>(publicationName) {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-            from(components["java"])
-        }
-        publishing.publications.map {
-            it.name
-        }.find {
-            it != "kotlinMultiplatform"
-        }
-    }
-}
-
-afterEvaluate {
-    configure<PublishingExtension> {
-        publications.all {
-            val mavenPublication = this as? MavenPublication
-            mavenPublication?.artifactId =
-                "${project.name}${"-$name".takeUnless { "kotlinMultiplatform" in name }.orEmpty()}"
-        }
-    }
-}
-configure<PublishingExtension> {
-    publications {
-        withType<MavenPublication> {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-        }
-    }
-    repositories {
-        // publish locally, then a github action pushes it to a different git repo where i'm using github pages as a maven repo
-        // publishToMavenLocal doesn't seem to work, it doesn't create the js and jvm publications for some reason.
-        // that's why we're running publish instead, and just setting the maven repo to a local file
-        maven("file://${System.getenv("GITHUB_WORKSPACE")}/maven")
     }
 }
