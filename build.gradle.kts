1ab9d497299ae353191600b67050394f579aaf39
DetachHead <detachhead@users.noreply.github.com>
2021-04-11 17:31:59 +1000

    sadf

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,3 +45,34 @@ kotlin {
         }
     }
 }
+
+if (System.getenv("JITPACK") == "true")
+    tasks["publishToMavenLocal"].doLast {
+        val version = System.getenv("VERSION")
+        val artifacts = publishing.publications.filterIsInstance<MavenPublication>().map { it.artifactId }
+
+        val dir: File = File(publishing.repositories.mavenLocal().url)
+            .resolve(project.group.toString().replace('.', '/'))
+
+        dir.listFiles { it -> it.name in artifacts }
+            .flatMap {
+                (
+                    it.listFiles { it -> it.isDirectory }?.toList()
+                        ?: emptyList<File>()
+                    ) + it.resolve("maven-metadata-local.xml")
+            }
+            .flatMap {
+                if (it.isDirectory) {
+                    it.listFiles { it ->
+                        it.extension == "module" ||
+                            "maven-metadata" in it.name ||
+                            it.extension == "pom"
+                    }?.toList() ?: emptyList()
+                } else listOf(it)
+            }
+            .forEach {
+                val text = it.readText()
+                println("Replacing ${project.version} with $version in $it")
+                it.writeText(text.replace(project.version.toString(), version))
+            }
+    }
